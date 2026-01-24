5bc4fe4329dd3cf287252ffcb80867c29e33a5ad
itsaraphap <itsaraphap.top@gmail.com>
2026-01-24 11:21:12 +0700

    ccc

---
+++ src/app/global-error.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+export const dynamic = 'force-dynamic';
+
+export default function GlobalError({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    return (
+        <html>
+            <body>
+                <h2>Something went wrong</h2>
+                <p>{error.message}</p>
+                <button onClick={() => reset()}>Try again</button>
+            </body>
+        </html>
+    );
+}
